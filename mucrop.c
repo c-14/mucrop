29dffe08d9e9691b012fe2fcdecf4afdab002035
c_14 <git@c-14.de>
2017-10-22 18:44:26 +0200

    Add crop support.

--- mucrop.c
+++ mucrop.c
@@ -26,8 +26,14 @@ struct mucrop_core {
 	size_t width;
 	size_t height;
 
+	int16_t start_x;
+	int16_t start_y;
+	int16_t stop_x;
+	int16_t stop_y;
+
 	bool quit;
 	bool wait;
+	bool crop;
 };
 
 #define RaiseWandException(wand, errlist) \
@@ -85,11 +91,95 @@ int read_image(struct mucrop_core *core, const char *filename)
 	return 0;
 }
 
+int start_crop(struct mucrop_core *core, xcb_button_press_event_t *ev)
+{
+	if (!(ev->detail & XCB_BUTTON_INDEX_1))
+		return 0;
+
+	core->start_x = ev->event_x;
+	core->start_y = ev->event_y;
+
+	return 1;
+}
+
+int stop_crop(struct mucrop_core *core, const char *src_filename, const char *dst_filename, xcb_button_release_event_t *ev)
+{
+	MagickBooleanType status;
+	ssize_t x, y;
+	size_t width, height;
+	double scale;
+
+	if (!(ev->detail & XCB_BUTTON_INDEX_1))
+		return 0;
+
+	core->stop_x = ev->event_x;
+	core->stop_y = ev->event_y;
+
+	if (core->start_x > core->stop_x) {
+		int16_t tmp = core->start_x;
+		core->start_x = core->stop_x;
+		core->stop_x = tmp;
+	}
+	if (core->start_y > core->stop_y) {
+		int16_t tmp = core->start_y;
+		core->start_y = core->stop_y;
+		core->stop_y = tmp;
+	}
+
+	if (core->start_x < core->window->xoff) {
+		x = 0;
+	} else {
+		x = core->start_x - core->window->xoff;
+	}
+	if (core->start_y < core->window->yoff) {
+		y = 0;
+	} else {
+		y = core->start_y - core->window->yoff;
+	}
+
+	if ((uint16_t)core->stop_x > core->window->xoff + core->width) {
+		width = core->width - x;
+	} else {
+		width = core->stop_x - (x + core->window->xoff);
+	}
+	if ((uint16_t)core->stop_y > core->window->yoff + core->height) {
+		height = core->height - y;
+	} else {
+		height = core->stop_y - (y + core->window->yoff);
+	}
+
+	if (width <= 0 || height <= 0)
+		return 0;
+
+	scale   = (double)core->o_width / (double)core->width;
+	x      *= scale;
+	width  *= scale;
+	scale   = (double)core->o_height / (double)core->height;
+	y      *= scale;
+	height *= scale;
+
+	status = MagickReadImage(core->wand, src_filename);
+	if (status == MagickFalse) {
+		RaiseWandException(core->wand, &core->errlist);
+		return -1;
+	}
+
+	MagickCropImage(core->wand, width, height, x, y);
+	MagickWriteImage(core->wand, dst_filename);
+
+	ClearMagickWand(core->wand);
+
+	return 1;
+}
+
 void handle_keypress(struct mucrop_core *core, xcb_key_press_event_t *key)
 {
 	switch (key->detail) {
 		case 0x35: // q
 			core->quit = true;
+			break;
+		case 0x42: // ESC
+			core->crop = false;
 			break;
 		default:
 			printf("%x\n", key->detail);
@@ -101,7 +191,8 @@ int main(int argc, const char *argv[])
 {
 	struct mucrop_core core = {};
 	xcb_generic_event_t *ev;
-	const char *filename = argv[1];
+	const char *src_filename = argv[1];
+	const char *dst_filename = argv[2];
 	struct timespec tp;
 	bool resize = false;
 	int ret = 0;
@@ -116,7 +207,7 @@ int main(int argc, const char *argv[])
 		goto fail;
 	}
 
-	ret = ping_image(&core, filename);
+	ret = ping_image(&core, src_filename);
 	if (ret != 0) {
 		goto fail;
 	}
@@ -132,7 +223,7 @@ int main(int argc, const char *argv[])
 	create_pixmap(&core.errlist, core.window, core.width, core.height);
 	create_gc(&core.errlist, core.window);
 
-	ret = read_image(&core, filename);
+	ret = read_image(&core, src_filename);
 	if (ret != 0)
 		goto fail;
 
@@ -153,7 +244,7 @@ int main(int argc, const char *argv[])
 			if (resize) {
 				clock_gettime(CLOCK_MONOTONIC, &now);
 				if (difftimespec(&now, &tp) > 500) {
-					if (read_image(&core, filename) != 0)
+					if (read_image(&core, src_filename) != 0)
 						goto fail;
 					load_image(&core.errlist, core.window, core.image, core.length, core.width, core.height);
 					core.wait = true;
@@ -167,8 +258,19 @@ int main(int argc, const char *argv[])
 				handle_keypress(&core, (xcb_key_press_event_t *)ev);
 				break;
 			case XCB_BUTTON_PRESS:
+				if (start_crop(&core, (xcb_button_press_event_t *)ev) > 0)
+					core.crop = true;
 				break;
 			case XCB_BUTTON_RELEASE:
+				if (core.crop) {
+					ret = stop_crop(&core, src_filename, dst_filename, (xcb_button_release_event_t *)ev);
+					if (ret == 0)
+						core.crop = false;
+					else if (ret < 0)
+						goto fail;
+					else if (ret > 0)
+						core.quit = true;
+				}
 				break;
 			case XCB_EXPOSE:
 				handle_expose(&core.errlist, core.window, core.width, core.height, (xcb_expose_event_t *)ev);
