8d4a7674e983a9ccb5c742ca908de54f39eb5451
c_14 <git@c-14.de>
2017-10-25 00:09:13 +0200

    Add support for multiple crop stages.
    Also replaces several booleans with state_flags and creates a new
    reload_image function that handles reloading the (potentially cropped)
    image.

--- mucrop.c
+++ mucrop.c
@@ -13,6 +13,21 @@
 #include "util/error.h"
 #include "util/time.h"
 
+typedef struct Point {
+	int16_t x;
+	int16_t y;
+} Point;
+
+enum mucrop_states {
+	MU_COMP = (1 << 1),
+	MU_CROP = (1 << 2),
+	MU_QUIT = (1 << 3),
+	MU_RESI = (1 << 4),
+	MU_SAVE = (1 << 5),
+	MU_UNDO = (1 << 6),
+	MU_WAIT = (1 << 7)
+};
+
 struct mucrop_core {
 	MagickWand *wand;
 	struct mu_window *window;
@@ -26,14 +41,11 @@ struct mucrop_core {
 	size_t width;
 	size_t height;
 
-	int16_t start_x;
-	int16_t start_y;
-	int16_t stop_x;
-	int16_t stop_y;
+	Point crop_origin;
+	size_t crop_width;
+	size_t crop_height;
 
-	bool quit;
-	bool wait;
-	bool crop;
+	uint16_t state_flags;
 };
 
 #define RaiseWandException(wand, errlist) \
@@ -81,7 +93,8 @@ int read_image(struct mucrop_core *core, const char *filename)
 	}
 
 	scale_to_window(&core->width, &core->height, core->window->width, core->window->height);
-	MagickResizeImage(core->wand, core->width, core->height, LanczosFilter);
+	if ((core->width != core->o_width) || (core->height != core->o_height))
+		MagickResizeImage(core->wand, core->width, core->height, LanczosFilter);
 
 	MagickSetImageFormat(core->wand, "bgra");
 	core->image = MagickGetImageBlob(core->wand, &core->length);
@@ -91,72 +104,124 @@ int read_image(struct mucrop_core *core, const char *filename)
 	return 0;
 }
 
-int start_crop(struct mucrop_core *core, xcb_button_press_event_t *ev)
+int reload_image(struct mucrop_core *core, const char *filename)
+{
+	MagickBooleanType status;
+	size_t width, height;
+
+	status = MagickReadImage(core->wand, filename);
+	if (status == MagickFalse) {
+		RaiseWandException(core->wand, &core->errlist);
+		return -1;
+	}
+
+	if (core->state_flags & MU_CROP) {
+		width  = core->width  = core->crop_width;
+		height = core->height = core->crop_height;
+		MagickCropImage(core->wand, core->width, core->height, core->crop_origin.x, core->crop_origin.y);
+	} else {
+		width  = core->width  = core->o_width;
+		height = core->height = core->o_height;
+	}
+
+	scale_to_window(&core->width, &core->height, core->window->width, core->window->height);
+	if ((core->width != width) || (core->height != height))
+		MagickResizeImage(core->wand, core->width, core->height, LanczosFilter);
+
+	MagickSetImageFormat(core->wand, "bgra");
+	core->image = MagickGetImageBlob(core->wand, &core->length);
+
+	ClearMagickWand(core->wand);
+
+	return load_image(&core->errlist, core->window, core->image, core->length, core->width, core->height);
+;
+}
+
+int bound_init(Point *bound_origin, xcb_button_press_event_t *ev)
 {
 	if (!(ev->detail & XCB_BUTTON_INDEX_1))
 		return 0;
 
-	core->start_x = ev->event_x;
-	core->start_y = ev->event_y;
+	bound_origin->x = ev->event_x;
+	bound_origin->y = ev->event_y;
 
 	return 1;
 }
 
-int stop_crop(struct mucrop_core *core, const char *src_filename, const char *dst_filename, xcb_button_release_event_t *ev)
+int bound_compute(struct mucrop_core *core, Point *bound_origin, xcb_button_release_event_t *ev)
 {
-	MagickBooleanType status;
-	ssize_t x, y;
-	size_t width, height;
-	double scale;
+	size_t x, y, width, height;
+	double scale_x, scale_y;
+	Point bound_end;
 
 	if (!(ev->detail & XCB_BUTTON_INDEX_1))
 		return 0;
 
-	core->stop_x = ev->event_x;
-	core->stop_y = ev->event_y;
+	bound_end.x = ev->event_x;
+	bound_end.y = ev->event_y;
 
-	if (core->start_x > core->stop_x) {
-		int16_t tmp = core->start_x;
-		core->start_x = core->stop_x;
-		core->stop_x = tmp;
+	if (bound_origin->x > bound_end.x) {
+		int16_t tmp = bound_origin->x;
+		bound_origin->x = bound_end.x;
+		bound_end.x = tmp;
 	}
-	if (core->start_y > core->stop_y) {
-		int16_t tmp = core->start_y;
-		core->start_y = core->stop_y;
-		core->stop_y = tmp;
+	if (bound_origin->y > bound_end.y) {
+		int16_t tmp = bound_origin->y;
+		bound_origin->y = bound_end.y;
+		bound_end.y = tmp;
 	}
 
-	if (core->start_x < core->window->xoff) {
+	if (bound_origin->x < core->window->xoff) {
 		x = 0;
 	} else {
-		x = core->start_x - core->window->xoff;
+		x = bound_origin->x - core->window->xoff;
 	}
-	if (core->start_y < core->window->yoff) {
+	if (bound_origin->y < core->window->yoff) {
 		y = 0;
 	} else {
-		y = core->start_y - core->window->yoff;
+		y = bound_origin->y - core->window->yoff;
 	}
 
-	if ((uint16_t)core->stop_x > core->window->xoff + core->width) {
+	if ((uint16_t)bound_end.x > core->window->xoff + core->width) {
 		width = core->width - x;
 	} else {
-		width = core->stop_x - (x + core->window->xoff);
+		width = bound_end.x - (x + core->window->xoff);
 	}
-	if ((uint16_t)core->stop_y > core->window->yoff + core->height) {
+	if ((uint16_t)bound_end.y > core->window->yoff + core->height) {
 		height = core->height - y;
 	} else {
-		height = core->stop_y - (y + core->window->yoff);
+		height = bound_end.y - (y + core->window->yoff);
 	}
 
 	if (width <= 0 || height <= 0)
 		return 0;
 
-	scale   = (double)core->o_width / (double)core->width;
-	x      *= scale;
-	width  *= scale;
-	scale   = (double)core->o_height / (double)core->height;
-	y      *= scale;
-	height *= scale;
+	if (core->state_flags & MU_CROP) {
+		x += core->crop_origin.x;
+		y += core->crop_origin.y;
+		scale_x = (double)core->crop_width / (double)core->width;
+		scale_y = (double)core->crop_height / (double)core->height;
+	} else {
+		scale_x = (double)core->o_width / (double)core->width;
+		scale_y = (double)core->o_height / (double)core->height;
+	}
+
+	x      *= scale_x;
+	width  *= scale_x;
+	y      *= scale_y;
+	height *= scale_y;
+
+	core->crop_origin.x = x;
+	core->crop_origin.y = y;
+	core->crop_width = width;
+	core->crop_height = height;
+
+	return 1;
+}
+
+int crop_image(struct mucrop_core *core, const char *src_filename, const char *dst_filename)
+{
+	MagickBooleanType status;
 
 	status = MagickReadImage(core->wand, src_filename);
 	if (status == MagickFalse) {
@@ -164,7 +229,7 @@ int stop_crop(struct mucrop_core *core, const char *src_filename, const char *ds
 		return -1;
 	}
 
-	MagickCropImage(core->wand, width, height, x, y);
+	MagickCropImage(core->wand, core->crop_width, core->crop_height, core->crop_origin.x, core->crop_origin.y);
 	MagickWriteImage(core->wand, dst_filename);
 
 	ClearMagickWand(core->wand);
@@ -176,13 +241,15 @@ void handle_keypress(struct mucrop_core *core, xcb_key_press_event_t *key)
 {
 	switch (key->detail) {
 		case 0x35: // q
-			core->quit = true;
+			core->state_flags |= MU_QUIT;
+			break;
+		case 0x3b: // w
+			core->state_flags |= MU_QUIT | MU_SAVE;
 			break;
 		case 0x42: // ESC
-			core->crop = false;
+			core->state_flags &= ~MU_COMP;
 			break;
 		default:
-			printf("%x\n", key->detail);
 			break;
 	}
 }
@@ -190,11 +257,11 @@ void handle_keypress(struct mucrop_core *core, xcb_key_press_event_t *key)
 int main(int argc, const char *argv[])
 {
 	struct mucrop_core core = {};
+	Point bound_origin = {};
 	xcb_generic_event_t *ev;
 	const char *src_filename = argv[1];
 	const char *dst_filename = argv[2];
 	struct timespec tp;
-	bool resize = false;
 	int ret = 0;
 
 	MagickWandGenesis();
@@ -231,24 +298,23 @@ int main(int argc, const char *argv[])
 
 	map_window(core.window);
 
-	core.wait = true;
-	while (!core.quit) {
+	core.state_flags |= MU_WAIT;
+	while (!(core.state_flags & MU_QUIT)) {
 		size_t sizes[4] = { core.width, core.height, core.o_width, core.o_height };
-		if (core.wait) {
+		if (core.state_flags & MU_WAIT) {
 			ev = xcb_wait_for_event(core.window->c);
 		} else {
 			ev = xcb_poll_for_event(core.window->c);
 		}
 		if (!ev) {
 			struct timespec now;
-			if (resize) {
+			if (core.state_flags & MU_RESI) {
 				clock_gettime(CLOCK_MONOTONIC, &now);
 				if (difftimespec(&now, &tp) > 500) {
-					if (read_image(&core, src_filename) != 0)
+					if (reload_image(&core, src_filename) != 0)
 						goto fail;
-					load_image(&core.errlist, core.window, core.image, core.length, core.width, core.height);
-					core.wait = true;
-					resize = false;
+					core.state_flags |= MU_WAIT;
+					core.state_flags &= ~MU_RESI;
 				}
 			}
 			continue;
@@ -258,18 +324,19 @@ int main(int argc, const char *argv[])
 				handle_keypress(&core, (xcb_key_press_event_t *)ev);
 				break;
 			case XCB_BUTTON_PRESS:
-				if (start_crop(&core, (xcb_button_press_event_t *)ev) > 0)
-					core.crop = true;
+				if (bound_init(&bound_origin, (xcb_button_press_event_t *)ev) > 0)
+					core.state_flags |= MU_COMP;
 				break;
 			case XCB_BUTTON_RELEASE:
-				if (core.crop) {
-					ret = stop_crop(&core, src_filename, dst_filename, (xcb_button_release_event_t *)ev);
-					if (ret == 0)
-						core.crop = false;
-					else if (ret < 0)
+				if (core.state_flags & MU_COMP) {
+					core.state_flags &= ~MU_COMP;
+					ret = bound_compute(&core, &bound_origin, (xcb_button_release_event_t *)ev);
+					if (ret > 0) {
+						core.state_flags |= MU_CROP;
+						if (reload_image(&core, src_filename) != 0)
+							goto fail;
+					} else if (ret < 0)
 						goto fail;
-					else if (ret > 0)
-						core.quit = true;
 				}
 				break;
 			case XCB_EXPOSE:
@@ -277,8 +344,8 @@ int main(int argc, const char *argv[])
 				break;
 			case XCB_CONFIGURE_NOTIFY:
 				if (resize_window(&core.errlist, core.window, sizes, (xcb_configure_notify_event_t *)ev)) {
-					core.wait = false;
-					resize = true;
+					core.state_flags &= ~MU_WAIT;
+					core.state_flags |= MU_RESI;
 					clock_gettime(CLOCK_MONOTONIC, &tp);
 				}
 				break;
@@ -286,6 +353,10 @@ int main(int argc, const char *argv[])
 				break;
 		}
 		free(ev);
+	}
+
+	if (core.state_flags & MU_SAVE) {
+		crop_image(&core, src_filename, dst_filename);
 	}
 
 fail:
